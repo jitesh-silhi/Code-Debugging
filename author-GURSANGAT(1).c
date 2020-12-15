0f6d979dd62affbe956d7d8581a9a2835cf1e9e4
rishusingh022 <rishusingh022@gmail.com>
2020-12-16 01:42:22 +0530

    inital commits

--- author-GURSANGAT(1).c
+++ author-GURSANGAT(1).c
@@ -1,13 +1,10 @@
-//Q2-CALC THE OUTPUT AND EXPLAIN
-
-
 # include <stdio.h>
 int main()
 {
    int i = 0;
    for (i=0; i<20; i++)
    {
-     switch(i)
+     switch(i);
      {
        case 0:
          i += 5;
@@ -19,14 +16,7 @@ int main()
          i += 4;
          break;
      }
-     printf("%d  ", i);
+     printf("%c  "; i);
    }
    return 0;
 }
-
-     
-//      HINTS/OPTIONS
-//A)5 10 15 20
-//B)7 12 17 22
-//C)16 21
-//D)Compiler Error
--- author-GURSANGAT(2).c
+++ author-GURSANGAT(2).c
@@ -1,22 +1,11 @@
-	
-//Point out the error in the program?
-
-
-#include<stdio.h>
+#include<stdi0.h>
 
 int main()
 {
     char ch;
     int i;
-    scanf("%c", &i);
-    scanf("%d", &ch);
-    printf("%c %d", ch, i);
+    scanf("%c", i);
+    scanf("%d", ch);
+    printf("%c %d", &ch, &i);
     return 0;
 }
-  
-  
-  //OPTIONS
-//[A].	Error: suspicious char to in conversion in scanf()
-//[B].  Error: we may not get input for second scanf() statement   (CORRECT)	
-//[C].	No error
-//[D].	None of above
